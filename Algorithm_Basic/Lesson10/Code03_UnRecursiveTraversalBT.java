295cc0de459cb7355657d3acd7e7c2d0723f52d7
songguangjin1314 <2096177261@qq.com>
2022-07-04 23:21:06 +0800

    add

--- Algorithm_Basic/Lesson10/Code03_UnRecursiveTraversalBT.java
+++ Algorithm_Basic/Lesson10/Code03_UnRecursiveTraversalBT.java
@@ -1,47 +1,49 @@
-package class10;
+package lesson10;
 
 import java.util.Stack;
 
 public class Code03_UnRecursiveTraversalBT {
-
-	public static class Node {
+	
+	public static class Node{
 		public int value;
 		public Node left;
 		public Node right;
-
+		
 		public Node(int v) {
 			value = v;
 		}
 	}
-
+	
+	// preface 
 	public static void pre(Node head) {
-		System.out.print("pre-order: ");
-		if (head != null) {
+		System.out.println("pre-order");
+		if(head != null) {
 			Stack<Node> stack = new Stack<Node>();
 			stack.add(head);
-			while (!stack.isEmpty()) {
+			while(!stack.isEmpty()) {
 				head = stack.pop();
-				System.out.print(head.value + " ");
-				if (head.right != null) {
+				System.out.println(head.value + " ");
+				if(head.right != null) {
 					stack.push(head.right);
 				}
-				if (head.left != null) {
+				if(head.left != null) {
 					stack.push(head.left);
 				}
-			}
+ 			}
 		}
 		System.out.println();
 	}
-
-	public static void in(Node cur) {
-		System.out.print("in-order: ");
-		if (cur != null) {
+	
+	// mid
+	public static void mid(Node cur) {
+		System.out.println("mid-order");
+		if(cur != null) {
 			Stack<Node> stack = new Stack<Node>();
-			while (!stack.isEmpty() || cur != null) {
-				if (cur != null) {
+			while(!stack.isEmpty() || cur != null) {
+				if(cur != null) {
 					stack.push(cur);
 					cur = cur.left;
-				} else {
+				}else {
 					cur = stack.pop();
 					System.out.print(cur.value + " ");
 					cur = cur.right;
@@ -50,69 +52,51 @@ public class Code03_UnRecursiveTraversalBT {
 		}
 		System.out.println();
 	}
-
-	public static void pos1(Node head) {
-		System.out.print("pos-order: ");
-		if (head != null) {
-			Stack<Node> s1 = new Stack<Node>();
-			Stack<Node> s2 = new Stack<Node>();
-			s1.push(head);
-			while (!s1.isEmpty()) {
-				head = s1.pop(); // 头 右 左
-				s2.push(head);
-				if (head.left != null) {
-					s1.push(head.left);
+	
+	// post
+	public static void pos(Node head) {
+		System.out.println("in-order");
+		if(head != null) {
+			Stack<Node> stack1 = new Stack<Node>();
+			Stack<Node> stack2 = new Stack<Node>();
+			stack1.push(head);
+			while(!stack1.isEmpty()) {
+				head = stack1.pop();
+				stack1.push(head);
+				if(head.left != null) {
+					stack1.push(head.left);
 				}
-				if (head.right != null) {
-					s1.push(head.right);
-				}
-			}
-			// 左 右 头
-			while (!s2.isEmpty()) {
-				System.out.print(s2.pop().value + " ");
-			}
-		}
-		System.out.println();
-	}
-
-	public static void pos2(Node h) {
-		System.out.print("pos-order: ");
-		if (h != null) {
-			Stack<Node> stack = new Stack<Node>();
-			stack.push(h);
-			Node c = null;
-			while (!stack.isEmpty()) {
-				c = stack.peek();
-				if (c.left != null && h != c.left && h != c.right) {
-					stack.push(c.left);
-				} else if (c.right != null && h != c.right) {
-					stack.push(c.right);
-				} else {
-					System.out.print(stack.pop().value + " ");
-					h = c;
+				if(head.right != head.right) {
+					stack2.push(head.right);
 				}
 			}
-		}
-		System.out.println();
+			while(!stack2.isEmpty()) {
+				head = stack2.pop();
+				System.out.print( head.value + " ");
+			}
+ 		}
 	}
-
-	public static void main(String[] args) {
-		Node head = new Node(1);
-		head.left = new Node(2);
-		head.right = new Node(3);
-		head.left.left = new Node(4);
-		head.left.right = new Node(5);
-		head.right.left = new Node(6);
-		head.right.right = new Node(7);
-
-		pre(head);
-		System.out.println("========");
-		in(head);
-		System.out.println("========");
-		pos1(head);
-		System.out.println("========");
-		pos2(head);
-		System.out.println("========");
-	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
