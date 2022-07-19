311d147777ab123af71e42622e568e7ca1d102d0
Dawn <2096177261@qq.com>
2022-07-19 21:59:41 +0800

    add

--- Algorithm_Basic/Lesson18/Code02_CardsInlLine.java
+++ Algorithm_Basic/Lesson18/Code02_CardsInlLine.java
@@ -8,8 +8,8 @@ public class Code02_CardsInlLine {
 		if(arr == null || arr.length  < 1) {
 			return 0;
 		}
-		int first = f1(arr, 0, arr.length);
-		int second = g1(arr, 0, arr.length);
+		int first = f1(arr, 0, arr.length - 1);
+		int second = g1(arr, 0, arr.length - 1);
 		return Math.max(first, second);
 	}
 	
@@ -36,13 +36,94 @@ public class Code02_CardsInlLine {
 		return Math.min(p1, p2);
 	}
 	
+	public static int win2(int[] arr) {
+		if(arr == null || arr.length < 1) {
+			return 0;
+		}
+		int N = arr.length;
+		int[][] fmap = new int[N][N];
+		int[][] gmap = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				fmap[i][j] = -1;
+				gmap[i][j] = -1;
+			}
+		}
+		int first = f2(arr, 0, arr.length - 1, fmap, gmap);
+		int second = g2(arr, 0, arr.length - 1, fmap, gmap);
+		return Math.max(first, second);
+	}
 	
-	
+	public static int f2(int[] arr, int L, int R, int[][] fmap, int[][] gmap) {
+		if(fmap[L][R] != -1) {
+			return fmap[L][R];
+		}
+		int ans = 0;
+		if(L == R) {
+			ans = arr[L];
+		}else {
+			int p1 = arr[L] + g2(arr, L + 1, R, fmap, gmap);
+			int p2 = arr[R] + g2(arr, L, R - 1, fmap, gmap);
+			ans = Math.max(p1, p2);
+		}
+		fmap[L][R] = ans;
+		return ans;
+	}
+	 public static int g2(int[] arr, int L, int R, int[][] fmap, int[][]gmap) {
+		 if(gmap[L][R] != -1) {
+			 return gmap[L][R];
+		 }
+		 int ans = 0;
+		 if(L != R) {
+			 int p1 = f2(arr, L + 1, R, fmap, gmap);
+			 int p2 = f2(arr, L, R - 1, fmap, gmap);
+			 ans = Math.min(p1, p2);
+		 }
+		 gmap[L][R] = ans;
+		 return ans;
+	 }
+	 
+	 // 动态优化
+	 public static int win3(int[] arr) {
+		 if(arr == null || arr.length < 1) {
+			 return 0;
+		 }
+		 int N = arr.length;
+		 int[][] fmap = new int[N][N];
+		 int[][] gmap = new int[N][N];
+		 for(int i = 0; i < N; i++) {
+			 fmap[i][i] = arr[i]; 
+		 }
+		 for(int startCol = 1; startCol < N; startCol++) {
+			 int L = 0;
+			 int R = startCol;
+			 while(R < N) {
+				 fmap[L][R] = Math.max(arr[L] + gmap[L + 1][R], arr[R] + gmap[L][R - 1]);
+				 gmap[L][R] = Math.min(fmap[L + 1][R], fmap[L][R - 1]);
+				 L++;
+				 R++;
+			 }
+		 }
+		 return Math.max(fmap[0][N - 1], gmap[0][N - 1]);
+	 }
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {100,23,24,43,54,43, 21};
+		System.out.println(win1(arr));
+		System.out.println(win2(arr));
+		System.out.println(win3(arr));
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
